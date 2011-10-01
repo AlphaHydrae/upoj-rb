ab6ace4c174c81bfb9ba4664e77b1585c411dbdd
AlphaHydrae <hydrae.alpha@gmail.com>
2011-10-01 15:59:14 +0200

    Included string inflections from active support.

--- lib/upoj-rb.rb
+++ lib/upoj-rb.rb
@@ -2,6 +2,7 @@ require 'active_support/core_ext/array/extract_options'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/try'
+require 'active_support/core_ext/string/inflections'
 
 module Upoj
   
