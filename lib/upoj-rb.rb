d76df9383ce246796d71e524688d25556d6872aa
AlphaHydrae <hydrae.alpha@gmail.com>
2011-10-01 18:31:18 +0200

    Added version to module.

--- lib/upoj-rb.rb
+++ lib/upoj-rb.rb
@@ -4,8 +4,12 @@ require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/try'
 require 'active_support/core_ext/string/inflections'
 
+dirname = File.dirname __FILE__
+deps_dir = File.join dirname, 'upoj-rb'
+VERSION_FILE = File.join dirname, '..', 'VERSION'
+
 module Upoj
-  
+  VERSION = File.open(VERSION_FILE, 'r').read
 end
 
-%w( opts signals ).each{ |dep| require File.join(File.dirname(__FILE__), 'upoj-rb', dep) }
+%w( opts signals ).each{ |dep| require File.join(deps_dir, dep) }
