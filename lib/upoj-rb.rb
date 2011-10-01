9a18c5fcba92a7d0f436158ab16f803db1d9db01
AlphaHydrae <hydrae.alpha@gmail.com>
2011-10-01 13:15:03 +0200

    Take ruby extensions directly from active support.

--- lib/upoj-rb.rb
+++ lib/upoj-rb.rb
@@ -1,5 +1,9 @@
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/object/try'
+
 module Upoj
   
 end
 
-%w( ext opts signals ).each{ |dep| require File.join(File.dirname(__FILE__), 'upoj-rb', dep) }
+%w( opts signals ).each{ |dep| require File.join(File.dirname(__FILE__), 'upoj-rb', dep) }
