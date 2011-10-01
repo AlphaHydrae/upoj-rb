e0206b8f1aa31dcbea123f3f0b7f0c36a40b8d43
AlphaHydrae <hydrae.alpha@gmail.com>
2011-10-01 13:16:53 +0200

    Included hash with indifferent access.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "http://rubygems.org"
 #   gem "activesupport", ">= 2.3.5"
 
 gem 'paint'
-gem 'active_support'
+gem 'active_support', '>= 2'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
--- lib/upoj-rb.rb
+++ lib/upoj-rb.rb
@@ -1,5 +1,6 @@
-require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/try'
 
 module Upoj
